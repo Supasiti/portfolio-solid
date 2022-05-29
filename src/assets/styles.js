e2d75bca4942c38086631effef9fff46e5159767
Thara Supasiti <sapiensis@gmail.com>
2022-05-29 12:34:46 +1000

    add styles

---
+++ src/assets/styles.js
@@ -0,0 +1,40 @@
+export const styles = {
+  color: {
+    primary: {
+      //sandy
+      lighter: '#F8C49A',
+      light: '#F6B581',
+      main: '#F4A261',
+      dark: '#C3824E',
+      darker: '#9C683E',
+    },
+    secondary: {
+      //teal
+      lighter: '#748991',
+      light: '#516B75',
+      main: '#264653',
+      dark: '#1E3842',
+      darker: '#182D35',
+    },
+    tertiary: {
+      //java
+      lighter: '#77C1B7',
+      light: '#55B1A5',
+      main: '#2A9D8F',
+      dark: '#227E72',
+      darker: '#1B655B',
+    },
+  },
+  shadow: {
+    [2]: '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)',
+    [4]: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
+    [8]: '0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)',
+  },
+  breakpoints: {
+    xs: 0,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
+  },
+};
